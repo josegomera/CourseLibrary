53417b77f440459edd96268d9a61489c584caa7f
Gomera <j.gomera@motorcredito.com.do>
2020-03-07 20:34:15 -0400

    Add project files.

---
+++ CourseLibrary.API/Controllers/AuthorController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace CourseLibrary.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibrary;
+        private readonly IMapper _mapper;
+
+        public AuthorController(ICourseLibraryRepository courseLibrary, IMapper mapper)
+        {
+            _courseLibrary = courseLibrary ??
+                throw new ArgumentNullException(nameof(courseLibrary));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthor()
+        {
+            var authors = _courseLibrary.GetAuthors();
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authors));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            var author = _courseLibrary.GetAuthor(id);
+
+            if (author == null)
+            {
+                return NotFound("The Author was not Found");
+            }
+
+            return Ok(_mapper.Map<AuthorDto>(author));
+        }
+    }
+}
---
+++ CourseLibrary.API/Profiles/AuthorProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Profiles
+{
+    public class AuthorProfiles : Profile
+    {
+        public AuthorProfiles()
+        {
+            CreateMap<Entities.Author, Models.AuthorDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+        }
+    }
+}
